612dd4bffcb0955b10da493d01be324bca37a805
Mark SchillI <Mark.Schill@cmschill.net>
2017-07-31 19:53:04 -0400

    Adds Travis CI integration

---
+++ build/travis.ps1
@@ -0,0 +1,10 @@
+
+
+# Install prerequisites
+Install-Module PSScriptAnalyzer -Scope CurrentUser -Force
+Install-Module Pester -Scope CurrentUser -Force
+
+
+
+#  Test
+Invoke-Pester -Script .\Test\*.tests.ps1
